17365045a0c93dac2dd200730c699bbcd3daa882
root <root@d41fa05a3401>
2020-04-22 22:03:29 +0000

    :smile:

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,7 +1,12 @@
 package _01_houses;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class Houses {
 	public void run() {
-		
+Robot robot = new Robot();
+		robot.setX(0);
+		robot.setY(550);
 	}
+	
 }
