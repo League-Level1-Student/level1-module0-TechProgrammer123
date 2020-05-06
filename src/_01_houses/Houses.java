371c73526526b0f6f6c20bc50f64592dbd742300
root <root@b992c146aa1a>
2020-05-06 21:55:08 +0000

    :smile:

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -3,10 +3,32 @@ package _01_houses;
 import org.jointheleague.graphical.robot.Robot;
 
 public class Houses {
+	Robot robot = new Robot("mini");
+
 	public void run() {
-Robot robot = new Robot();
-		robot.setX(0);
-		robot.setY(550);
+robot.setX(25);
+robot.setY(500);
+for (int i = 0; i < 10; i++) {
+	building();
+}
+
+	}
+	public void building() {
+		
+		robot.setSpeed(100);
+		robot.penDown();
+		robot.move(100);
+		robot.turn(90);
+		robot.move(25);
+		robot.turn(90);
+		robot.move(100);
+		robot.turn(-90);
+		robot.setPenColor(0,255,0);
+		robot.move(20);
+		robot.turn(-90);
+		robot.setPenColor(0,0,0);
+		
 	}
 	
-}
+	}
+
