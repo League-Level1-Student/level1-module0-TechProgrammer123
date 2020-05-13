fba074280b47d98610d00822c4a841c76053e1c2
root <root@482c5f235f3c>
2020-05-13 21:56:08 +0000

    :smile:

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,5 +1,7 @@
 package _01_houses;
 
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class Houses {
@@ -8,20 +10,32 @@ public class Houses {
 	public void run() {
 robot.setX(25);
 robot.setY(500);
-for (int i = 0; i < 10; i++) {
-	building();
+
+for (int i = 0; i < 2; i++) {
+	building(20);
+	building(40);
+	building(80);
+	building(30);
+	building(90);
+	building(10);
+	building(50);
+	building(70);
+	building(10);
 }
 
 	}
-	public void building() {
-		
+	public void building(String height) {
+		int Height = 120;
+		if (height.equals("small")) {
+			
+		}
 		robot.setSpeed(100);
 		robot.penDown();
-		robot.move(100);
+		robot.move(height);
 		robot.turn(90);
 		robot.move(25);
 		robot.turn(90);
-		robot.move(100);
+		robot.move(height);
 		robot.turn(-90);
 		robot.setPenColor(0,255,0);
 		robot.move(20);
@@ -29,6 +43,7 @@ for (int i = 0; i < 10; i++) {
 		robot.setPenColor(0,0,0);
 		
 	}
+	
 	
 	}
 
