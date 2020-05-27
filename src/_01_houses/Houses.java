5611ee593a41996eed4c0babcc5f6618c6e17f52
root <root@d819824a79fc>
2020-05-27 21:56:54 +0000

    :smile:

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -12,22 +12,18 @@ robot.setX(25);
 robot.setY(500);
 
 for (int i = 0; i < 3; i++) {
-	building("small");
+	drawPointyRoof("small");
 	building("large");
-	building("medium");
+	drawPointyRoof("medium");
+	
 
 }
 
 	}
 	public void building(String height) {
 		int Height = 120;
-		if (height.equals("small")) {
-			Height = 60;
-		}
-		else if (height.equals("medium")) {
-			Height = 120;
-		}
-		else if (height.equals("large")) {
+	
+		if (height.equals("large")) {
 			Height = 250;
 		}
 		robot.setSpeed(100);
@@ -43,6 +39,37 @@ for (int i = 0; i < 3; i++) {
 		robot.turn(-90);
 		robot.setPenColor(0,0,0);
 		
+	}
+	public void drawPointyRoof (String height) {
+		int Height = 120;
+		if (height.equals("small")) {
+			Height = 60;
+		}
+		else if (height.equals("medium")) {
+			Height = 120;
+		}
+		robot.setSpeed(100);
+		robot.penDown();
+		robot.move(Height);
+		robot.turn(90);
+		robot.move(25);
+		//pointy roof starts here
+		robot.turn(-120);
+		robot.move(25);
+		robot.turn(-120);
+		robot.move(25);
+		robot.turn(-120);
+		robot.move(25);
+		//pointy roof ends here
+		robot.turn(90);
+		robot.move(Height);
+		robot.turn(-90);
+		robot.setPenColor(0,255,0);
+		robot.move(20);
+		robot.turn(-90);
+		robot.setPenColor(0,0,0);
+		
+	
 	}
 	
 	
