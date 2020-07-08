ac26c53a446106fcdc11eca6dcec9d11ce988148
TechProgrammer123 <43911240+TechProgrammer123@users.noreply.github.com>
2020-07-08 14:53:55 -0700

    :smile:

--- src/_04_drum_kit/DrumKit.java
+++ src/_04_drum_kit/DrumKit.java
@@ -45,19 +45,19 @@ frame.add(panel);
 String drum = "EVO622BKSi-large.jpg";
 		// 9. Edit the next line to use your String variable
 		// drumLabelWithImage = createLabelImage(drumImageString);
-
+JLabel drumLabelWithImage = createLabelImage(drum);
 		// 10. Add the image to the panel
-
+panel.add(drumLabelWithImage);
 		// 11. Set the layout of the panel to "new GridLayout()"
-
+panel.setLayout(new GridLayout());
 		// 12. call the pack() method on the frame. Run your program. Do you see
 		// your drum image?
-
+frame.pack();
 		// 13. add this mouse listener to drumLabelWithImage
-
+drumLabelWithImage.addMouseListener(this);
 		// 18. Add more images to make a drumkit. Remember to add this mouse
 		// listener to each one.
-
+String snare = "";
 	}
 
 	public void mouseClicked(MouseEvent e) {
