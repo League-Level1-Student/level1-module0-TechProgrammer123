793f906a21255214ae35e6a4b400afacdc88f3d8
diamo <diamo@LAPTOP-CHQOKLU7.home>
2020-07-22 14:55:54 -0700

    :smile:

--- src/_05_body_part_quiz/BodyPartQuiz.java
+++ src/_05_body_part_quiz/BodyPartQuiz.java
@@ -51,7 +51,7 @@ public class BodyPartQuiz {
 	private void startQuiz() {
 
 		// 1. Make an int variable to hold the score.
-
+int score = 0;
 		// 2. Set the size of the window in the initializeGui() method 
 
 		// 4. Ask the user who this person is and store their answer
@@ -59,18 +59,44 @@ public class BodyPartQuiz {
 
 		// 5. Check their answer. If they guessed correctly:
 		// -- Tell them they are right and increase the score by 1
-
+if (guess.equals("Arnold")) {
+	score = +1;
+}
 		// 6. Otherwise:
 		// -- Tell them they are wrong and who the person is
-
+else {
+	JOptionPane.showMessageDialog(null, "Wrong, it's Arnold");
+}
 		// 7. Use the showNextImage() method below to get the next image
 		showNextImage();
 
 		// 8. .... repeat 4-7 for all your images.....
-
+		String guess2 = JOptionPane.showInputDialog("who is this?");
+		if (guess2.equals("Leonardo")) {
+			score = +1;
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "Wrong, it's Leonardo");
+		}
+		showNextImage();
+		String guess3 = JOptionPane.showInputDialog("who is this?");
+		if (guess3.equals("Morgan")) {
+			score = +1;
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "Wrong, it's Morgan");
+			showNextImage();
+			String guess4 = JOptionPane.showInputDialog("who is this?");
+			if (guess4.equals("Jack")) {
+				score = +1;
+			}
+			else {
+				JOptionPane.showMessageDialog(null, "Wrong, it's Jack");
+			}
 		// 9. Show them their current score
-
-	}
+JOptionPane.showMessageDialog(null, "Your score is"+score);
+		
+	
 
 	public void showNextImage() {
 		panel.removeAll();
