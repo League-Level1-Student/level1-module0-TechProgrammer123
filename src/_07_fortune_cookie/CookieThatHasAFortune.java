84fe895028ffbebb51e8a66c466e06a627cb81cc
diamo <diamo@LAPTOP-CHQOKLU7.home>
2020-08-19 14:56:47 -0700

    :smile:

---
+++ src/_07_fortune_cookie/CookieThatHasAFortune.java
@@ -0,0 +1,14 @@
+package _07_fortune_cookie;
+
+import javax.swing.JFrame;
+
+public class CookieThatHasAFortune {
+
+
+
+public void showButton() {
+	// TODO Auto-generated method stub
+	
+	JFrame frame = new JFrame();
+	 frame.setVisible(true);
+}}
