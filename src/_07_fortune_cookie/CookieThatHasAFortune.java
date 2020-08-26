f8e9d083ebf167a00f07ce617e869c86101ea235
diamo <diamo@LAPTOP-CHQOKLU7.home>
2020-08-26 14:56:16 -0700

    :smile:

--- src/_07_fortune_cookie/CookieThatHasAFortune.java
+++ src/_07_fortune_cookie/CookieThatHasAFortune.java
@@ -1,8 +1,14 @@
 package _07_fortune_cookie;
 
-import javax.swing.JFrame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
-public class CookieThatHasAFortune {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class CookieThatHasAFortune implements ActionListener {
 
 
 
@@ -11,4 +17,39 @@ public void showButton() {
 	
 	JFrame frame = new JFrame();
 	 frame.setVisible(true);
-}}
+	 JButton button = new JButton();
+	    frame.add(button);
+	    frame.pack();
+	    button.addActionListener(this);
+}
+
+@Override
+public void actionPerformed(ActionEvent arg0) {
+	// TODO Auto-generated method stub
+	JOptionPane.showMessageDialog(null, "Woohoo");
+	int rand = new Random().nextInt(5);
+	if (rand == 0) {
+		JOptionPane.showMessageDialog(null, "A cookie will show up at your door in approximitley 45.972840020705302 seconds");
+	}
+	if (rand == 1) {
+		JOptionPane.showMessageDialog(null, "You will have a fortune");
+	 	
+	}
+	 if (rand == 2) {
+		JOptionPane.showMessageDialog(null, "If you read this you must go step in water with socks on or else you will go 'bye bye'" );
+	}
+	if (rand == 3) {
+		JOptionPane.showMessageDialog(null, "You will go get french fry or draw 25");
+	}
+	if (rand == 4) {
+		JOptionPane.showMessageDialog(null, "Die or draw 25");
+	}
+}
+
+
+
+
+
+
+
+}
